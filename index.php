6aeae34feb024c173e80eadfd735cdf205bd89eb
chitralada <52810745+chitralada@users.noreply.github.com>
2019-07-15 13:50:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
  // Get message from Line API 
  $content = file_get_contents('php://input');
  $events = json_decode($content, true);
+ 
  if (!is_null($events['events'])) { 
  // Loop through each event 
  foreach ($events['events'] as $event) { 
